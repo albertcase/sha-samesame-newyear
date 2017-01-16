a96946efa84cb4be39cfced10efa27ce7f7101a6
evelyn_zhang <1054982653@qq.com>
2017-01-16 16:31:49 +0800

    animation

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -11,38 +11,37 @@
         //this.firstStop = true;
         this.floor1_imageArray = [];
         this.facade_imageArray = [];
+        this.floor2_before_imageArray = [];
+        this.floor3_afterArrow_imageArray = [];
+        this.suprise_imageArray = [];
         var ArrLength=120;
-        var newstring='',newstring2='';
-        for(var i=0;i<ArrLength;i++){
+        var newstring='',newstring2='',newstring3='',newstring4='',newstring5='';
+        for(var i=0;i<ArrLength;i=i+6){
             if(i<10){
                 newstring='src/dist/images/'+'floor_withoutbg/1floorwithoutbg_0000'+i+'.png';
                 newstring2='src/dist/images/'+'facade_alone/facadealone_0000'+i+'.png';
+                newstring3='src/dist/images/'+'2before/2before_0000'+i+'.png';
+                newstring4='src/dist/images/'+'3afterarrow/3afterarrow_0000'+i+'.png';
+                newstring5='src/dist/images/'+'endingpage_frame2/endingpage_0000'+i+'.jpg';
             }else if(i>9 && i<100){
                 newstring='src/dist/images/'+'floor_withoutbg/1floorwithoutbg_000'+i+'.png';
                 newstring2='src/dist/images/'+'facade_alone/facadealone_000'+i+'.png';
+                newstring3='src/dist/images/'+'2before/2before_000'+i+'.png';
+                newstring4='src/dist/images/'+'3afterarrow/3afterarrow_000'+i+'.png';
+                newstring5='src/dist/images/'+'endingpage_frame2/endingpage_000'+i+'.jpg';
             }else{
                 newstring='src/dist/images/'+'floor_withoutbg/1floorwithoutbg_00'+i+'.png';
                 newstring2='src/dist/images/'+'facade_alone/facadealone_00'+i+'.png';
+                newstring3='src/dist/images/'+'2before/2before_00'+i+'.png';
+                newstring4='src/dist/images/'+'3afterarrow/3afterarrow_00'+i+'.png';
+                newstring5='src/dist/images/'+'endingpage_frame2/endingpage_00'+i+'.jpg';
             }
             this.floor1_imageArray.push(newstring);
             this.facade_imageArray.push(newstring2);
+            this.floor2_before_imageArray.push(newstring3);
+            this.floor3_afterArrow_imageArray.push(newstring4);
+            this.suprise_imageArray.push(newstring5);
         };
-
-        //this.facade_imageArray = [];
-        //var newString2 = '';
-        //for(var i=0;i<120;i++){
-        //    if(i<10){
-        //        newString2='src/dist/images/'+'facade_alone/facadealone_0000'+i+'.png';
-        //    }else if(i>9 && i<100){
-        //        newString2='src/dist/images/'+'facade_alone/facadealone_000'+i+'.png';
-        //    }else{
-        //        newString2='src/dist/images/'+'facade_alone/facadealone_00'+i+'.png';
-        //    }
-        //    this.facade_imageArray.push(newString2);
-        //};
-
-
-        //console.log(floor1_imageArray);
 
 
     };
@@ -55,21 +54,7 @@
         var imagesArray = [
             //baseurl+'logo.png',
         ];
-        imagesArray = imagesArray.concat(self.floor1_imageArray).concat(self.facade_imageArray);
-        //console.log(imagesArray);
-        //var floor1_bg = [];
-        //append floor 1 image sequece to imageArray
-        //for(var x=0;x<120;x++){
-        //    var newstring = '';
-        //    if(x<10){
-        //        newstring=baseurl+'floor_withoutbg/1floorwithoutbg_0000'+x+'.png';
-        //    }else if(x>9 && x<100){
-        //        newstring=baseurl+'floor_withoutbg/1floorwithoutbg_000'+x+'.png';
-        //    }else{
-        //        newstring=baseurl+'floor_withoutbg/1floorwithoutbg_00'+x+'.png';
-        //    }
-        //    imagesArray.push(newstring);
-        //}
+        imagesArray = imagesArray.concat(self.floor1_imageArray).concat(self.facade_imageArray).concat(self.floor2_before_imageArray).concat(self.floor3_afterArrow_imageArray).concat(self.suprise_imageArray);
         var i = 0;
         new preLoader(imagesArray, {
             onProgress: function(){
@@ -83,9 +68,9 @@
                 //
                 $('.preload').remove();
                 $('.container').addClass('fade');
-                self.welcomePage();
+                //self.welcomePage();
                 //self.doGame();
-                //self.getSurprise();
+                self.getSurprise();
 
             }
         });
@@ -100,12 +85,12 @@
         Common.gotoPin(0);
         var j=0;
         var reqAnimateHome = new reqAnimate($('#pin-home .content img'),{
-            fps: 30,
-            totalFrames: 120,
-            //time: 0,
+            fps: 6,
+            totalFrames: 20,
+            time: 2,
             processAnimation: function(){
                 var imgName = self.facade_imageArray[j];
-                if(j>118){
+                if(j>self.facade_imageArray.length-2){
                     j=0;
                 }
                 j++;
@@ -129,7 +114,8 @@
     controller.prototype.doGame = function(){
         var self = this;
         Common.gotoPin(1);
-        self.animateForFloor3();
+        self.animateForFloor2Before();
+        self.animateForFloor1();
 
         var container = $('.pin-content');
         var scrren_1 = $('.screen-1'),
@@ -143,21 +129,20 @@
         var firstPosX = container.width()/2 - $('.role .role-progress').width()/ 2,
             firstLevelPosX = $('#floor1 .level')[0].offsetLeft - chickenLength;
 
-        var floor2PosY = $('#floor1').height()+10,
-            floor3PosY = $('#floor1').height()*2+20;
+        var floor2LevelPosX =$('#floor2').width()*0.4,
+            floor1PosY = scrren_1[0].offsetTop + scrren_1.height() - $('.role').height(),
+            floor2PosY = scrren_2[0].offsetTop + scrren_2.height() - $('.role').height(),
+            floor3PosY = 0;
+
+        var floor3PosXEnd = $('#floor2').width()*0.77 - $(ele).width()*0.5;
         var step = 10;
         console.log(firstLevelPosX);
-        self.startMove(ele,minPosX,firstLevelPosX,step);
-        //var aaa = setTimeout(function(){
-        //    self.forbiddenMove();
-        //},4000);
+        $('.role').css('top',floor1PosY);
 
-        //ele.addEventListener('animationstart',function(){
-        //    console.log('start');
-        //});
+        self.startMove(ele,minPosX,firstLevelPosX,step);
+
         //self.curStep = 5;
         ele.addEventListener('transitionend',function(){
-
 
             switch (self.curStep)
             {
@@ -177,13 +162,13 @@
                     break;
                 case 1:
                     console.log("floor1: from middle to  right");
-                    $('.role').css('bottom',floor2PosY);
+                    $('.role').css('top',floor2PosY);
                     $(ele).addClass('change');
                     break;
                 case 2:
                     console.log("floor1-2: from floor1 right to floor2 right");
-                    $(ele).css('left',firstLevelPosX);
-
+                    $(ele).css('left',floor2LevelPosX);
+                    $('.role').addClass('floor2mask');
                     break;
                 case 3:
                     console.log("floor2: from right to middle");
@@ -200,13 +185,13 @@
                     break;
                 case 4:
                     console.log("floor2: from middle to left");
-                    $('.role').css('bottom',floor3PosY);
+                    $('.role').css('top',floor3PosY);
                     $(ele).removeClass('change');
                     break;
                 case 5:
                     console.log("floor2-3: from floor2 left to floor3 left");
-                    $('.role').css('bottom',floor3PosY).addClass('floor3mask');
-                    $(ele).css('left',firstLevelPosX+10);
+                    $('.role').css('top',floor3PosY).addClass('floor3mask');
+                    $(ele).css('left',firstLevelPosX);
                     break;
                 case 6:
                     console.log("floor3: from floor3 left to middle");
@@ -215,26 +200,31 @@
                         if(self.curStep ==7){
                             $('#floor3 .dialogue').removeClass('show');
                             //after animation
-                            $('#floor3 .level').addClass('after');
-                            $('#floor3 .level img').attr('src','/src/dist/images/1eventafter2.gif');
-                            $(ele).css('left',firstLevelPosX+50);
+                            //$('#floor3 .level').addClass('after');
+                            //$('#floor3 .level img').attr('src','/src/dist/images/1eventafter2.gif');
+                            $(ele).css('left',floor3PosXEnd-20);
                         }
                     });
                     break;
                 case 7:
                     console.log("floor3: from floor3 middle to right");
                     //transform the element
-                    $(ele).addClass('change').css({
-                        left:firstLevelPosX+100,
-                        bottom:0,
-                    });
+                    self.animateForFloor3();
+                    //$(ele).css({
+                    //    left:firstLevelPosX+250,
+                    //    bottom:0,
+                    //});
+                    $(ele).css('left',floor3PosXEnd);
                     break;
                 case 8:
                     console.log("floor3: from floor3 right to out");
                     $(ele).addClass('changedirection').css({
-                        left:firstLevelPosX+250,
-                        bottom:60,
+                        left:floor3PosXEnd+$(ele).width(),
+                        bottom:$(ele).height(),
                     });
+                    break;
+                case 9:
+                    console.log("floor3: from floor3 out to hide");
                     self.getSurprise();
                     break;
             };
@@ -262,16 +252,39 @@
 
 
     };
-    controller.prototype.animateForFloor3 = function () {
+    controller.prototype.animateForFloor2Before = function () {
+        var self = this;
+        var j = 0;
+        var reqAnimateFloor2Before = new reqAnimate($('#floor2 .l-bg img'),{
+            fps: 6,
+            totalFrames: 20,
+            //time: 0,
+            processAnimation: function(){
+                var imgName = self.floor2_before_imageArray[j];
+                if(j>self.floor2_before_imageArray.length-2){
+                    j=0;
+                }
+                j++;
+                $('#floor2 .l-bg img').attr('src',imgName);
+            },
+            doneAnimation: function(){
+
+                //show box and letter
+            }
+        });
+        reqAnimateFloor2Before.start();
+
+    };
+    controller.prototype.animateForFloor1 = function () {
         var self = this;
         var j = 0;
         var reqAnimateNow = new reqAnimate($('#floor1 .l-bg img'),{
-            fps: 30,
-            totalFrames: 120,
+            fps: 6,
+            totalFrames: 20,
             //time: 0,
             processAnimation: function(){
                 var imgName = self.floor1_imageArray[j];
-                if(j>118){
+                if(j>self.floor1_imageArray.length-2){
                     j=0;
                 }
                 j++;
@@ -284,7 +297,31 @@
         });
         reqAnimateNow.start();
 
-    }
+    };
+
+    controller.prototype.animateForFloor3 = function () {
+        var self = this;
+        var j = 0;
+        var reqAnimateFloor3 = new reqAnimate($('#floor3 .arrow img'),{
+            fps: 6,
+            totalFrames: 20,
+            time: 1,
+            processAnimation: function(){
+                var imgName = self.floor3_afterArrow_imageArray[j];
+                if(j>self.floor3_afterArrow_imageArray.length-2){
+                    j=0;
+                }
+                j++;
+                $('#floor3 .arrow img').attr('src',imgName);
+            },
+            doneAnimation: function(){
+
+                //show box and letter
+            }
+        });
+        reqAnimateFloor3.start();
+
+    };
 
     //the element start move
     controller.prototype.startMove = function(e,startPos,endPos,step){
@@ -321,17 +358,47 @@
     controller.prototype.getSurprise = function(){
         var self = this;
         Common.gotoPin(2);
+        self.surpriseBgAni();
         //click ask btn, go exchange
-        $('.result-ask .btn').on('click', function(){
+        $('.result-ask .btn').on('touchstart', function(){
             $('.result-ask').removeClass('show');
             $('.result-1').addClass('show');
         });
 
         //click go to qrcode part
-        $('.result .btn-exchange').on('click', function(){
+        $('.result .btn-exchange').on('touchstart', function(){
             $('.result').removeClass('show');
             $('.qrcode').addClass('show');
         });
+
+        //qrcode
+        $('.qrcode .btn-exchange').on('touchstart', function(){
+            self.welcomePage();
+        });
+
+    };
+
+    controller.prototype.surpriseBgAni = function () {
+        var self = this;
+        var j = 0;
+        var reqAnimateSurpriseBgAni = new reqAnimate($('#pin-surprise .result-wrap img'),{
+            fps: 6,
+            totalFrames: 20,
+            //time: 1,
+            processAnimation: function(){
+                var imgName = self.suprise_imageArray[j];
+                if(j>self.suprise_imageArray.length-2){
+                    j=0;
+                }
+                j++;
+                $('#pin-surprise .result-wrap img').attr('src',imgName);
+            },
+            doneAnimation: function(){
+
+                //show box and letter
+            }
+        });
+        reqAnimateSurpriseBgAni.start();
 
     };
 
