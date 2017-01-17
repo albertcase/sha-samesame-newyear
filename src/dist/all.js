b07c8f52aea8eb444a4783e2c3f257c737440cb8
evelyn_zhang <1054982653@qq.com>
2017-01-17 17:51:39 +0800

    wx

--- src/dist/all.js
+++ src/dist/all.js
@@ -679,26 +679,25 @@ Api = {
         //    menuList: ["menuItem:share:appMessage","menuItem:share:timeline","menuItem:share:qq","menuItem:share:weiboApp","menuItem:share:facebook","menuItem:share:QZone","menuItem:copyUrl","menuItem:openWithQQBrowser","menuItem:openWithSafari","menuItem:share:email"] // 要显示的菜单项，所有menu项见附录3
         //});
         wx.onMenuShareAppMessage({
-            title: '与刘嘉玲、娜扎一起领略ROSSO VALENTINO梦幻之作',
-            desc: '红色SPIKE铆钉链条包，Valentino微信独家限量发售。',
-            link: 'http://rossovalentino.samesamechina.com/event/',
-            imgUrl: window.location.origin+'/src/images/share.jpg',
+            title: '一七送你好时机 wx',
+            desc: '一七送你好时机',
+            link: window.location.href,
+            imgUrl: window.location.origin+'/src/dist/images/share.png',
             type: '',
             dataUrl: '',
             success: function () {
-                //    success
-                _hmt.push(['_trackEvent', 'btn-weixin', 'share', 'success']);
+
 
             },
             cancel: function () {
             }
         });
         wx.onMenuShareTimeline({
-            title: '与刘嘉玲、娜扎一起领略ROSSO VALENTINO梦幻之作',
-            link: 'http://rossovalentino.samesamechina.com/event/',
-            imgUrl: window.location.origin+'/src/images/share.jpg',
+            title: '一七送你好时机 wx',
+            link: window.location.href,
+            imgUrl: window.location.origin+'/src/dist/images/share.png',
             success: function () {
-                _hmt.push(['_trackEvent', 'btn-weixin', 'share', 'success']);
+
             },
             cancel: function () {
 
